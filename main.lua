1a2b2df5366730f99c20399579f9f78598deae5f
Frijoles Y Queso <135540428+FrijolesYQueso@users.noreply.github.com>
2024-02-28 18:25:26 -0600

    remove yumi

--- main.lua
+++ main.lua
@@ -8,7 +8,6 @@
 
 local E_MODEL_WHITEY = smlua_model_util_get_id("Whitey_geo")
 local E_MODEL_HH = smlua_model_util_get_id("HH_geo")
-local E_MODEL_JYUMI = smlua_model_util_get_id("JYumi_geo")
 
 local TEXT_MOD_NAME = "Holiday Bundle"
 
@@ -39,38 +38,17 @@ local VOICETABLE_WHITEY = {
     [CHAR_SOUND_YAHOO_WAHA_YIPPEE] = {'Dont-Forget.ogg', 'Wah.ogg', 'YEAAAH.ogg'},
     [CHAR_SOUND_YAH_WAH_HOO] = {'YEAAAH.ogg', 'Wah.ogg', 'AAA.ogg'},
     [CHAR_SOUND_YAWNING] = nil,
-}
-local VOICETABLE_JYUMI = {
-    [CHAR_SOUND_ATTACKED] = 'YumiHurtHard.ogg',
-    [CHAR_SOUND_DROWNING] = 'YumiDrown.ogg',
-    [CHAR_SOUND_DYING] = 'YumiDrown.ogg',
-    [CHAR_SOUND_EEUH] = 'YumiPickUp.ogg',
-    [CHAR_SOUND_HERE_WE_GO] = 'YumiWin.ogg',
-    [CHAR_SOUND_IMA_TIRED] = 'YumiTired.ogg',
-    [CHAR_SOUND_LETS_A_GO] = 'YumiLetsGo.ogg',
-    [CHAR_SOUND_MAMA_MIA] = 'YumiMamaMia.ogg',
-    [CHAR_SOUND_ON_FIRE] = 'YumiBurn.ogg',
-    [CHAR_SOUND_SO_LONGA_BOWSER] = 'YumiSoLong.ogg',
-    [CHAR_SOUND_UH] = 'YumiHurt.ogg',
-    [CHAR_SOUND_UH2] = 'YumiHurtHard.ogg',
-    [CHAR_SOUND_WAAAOOOW] = 'YumiScreamFall.ogg',
-    [CHAR_SOUND_WHOA] = 'YumiPickUp.ogg',
-}
 
 if _G.charSelectExists then
     CT_WHITEY = _G.charSelect.character_add("Whitey", {"Whitey Duvall from Adam Sandler's Eight Crazy Nights", "is now here."}, "Trashcam", {r = 255, g = 200, b = 200}, E_MODEL_WHITEY, CT_MARIO)
     CT_HH = _G.charSelect.character_add("HH", {"Save on everything at Christmas in July", "H-H-H-H-H-H-HHGregg", "Panasonic Blu-Ray, 99 dollars", "H-H-H-H-H-H-HHGregg", "32-inch LCD TV, 299", "LG 42-inch HDTV, 489", "Everything's on sale during Christmas in July", "H-H-H-H", "A snowball in July?", "HHGregg!"}, "Garlicker", {r = 255, g = 200, b = 200}, E_MODEL_HH, CT_MARIO)
-    CT_JYUMI = _G.charSelect.character_add("Jolly Yumi", {"A girl from New York.", "She seems to feel pretty", "jolly today."}, "Frijoles.z64", {r = 54, g = 91, b = 212}, E_MODEL_JYUMI, CT_LUIGI)
 
     _G.charSelect.character_add_voice(E_MODEL_WHITEY, VOICETABLE_WHITEY)
-    _G.charSelect.character_add_voice(E_MODEL_JYUMI, VOICETABLE_JYUMI)
     hook_event(HOOK_CHARACTER_SOUND, function (m, sound)
         if _G.charSelect.character_get_voice(m) == VOICETABLE_WHITEY then return _G.charSelect.voice.sound(m, sound) end
-        if _G.charSelect.character_get_voice(m) == VOICETABLE_JYUMI then return _G.charSelect.voice.sound(m, sound) end
     end)
     hook_event(HOOK_MARIO_UPDATE, function (m)
         if _G.charSelect.character_get_voice(m) == VOICETABLE_WHITEY then return _G.charSelect.voice.snore(m) end
-        if _G.charSelect.character_get_voice(m) == VOICETABLE_JYUMI then return _G.charSelect.voice.snore(m) end
     end)
 else
     djui_popup_create("\\#ffffdc\\\n"..TEXT_MOD_NAME.."\nRequires the Character Select Mod\nto use as a Library!\n\nPlease turn on the Character Select Mod\nand Restart the Room!", 6)
